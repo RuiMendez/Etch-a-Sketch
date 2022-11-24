bf23d72c09d2732a430013f087a964f2645625d6
Rui <86rui.mendes@gmail.com>
2022-11-24 14:02:09 +0000

    hover effect implemented successfully

--- main.js
+++ main.js
@@ -8,10 +8,14 @@ function createGrid(amountOfGrids) {
     for (let i = 0; i < amountOfGrids; i++) {
         const row = document.createElement("div");
         row.classList.add("grid-row");
+
         for (let j = 0; j < amountOfGrids; j++) {
             const columnBox = document.createElement("div");
             columnBox.classList.add("grid-box");
             row.appendChild(columnBox);
+            columnBox.addEventListener("mouseenter", () => {
+                columnBox.style.backgroundColor = "white";
+            });
         }
         sketchContainer.appendChild(row);
     }
