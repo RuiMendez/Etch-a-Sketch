d6381e228e237ae6d39baa44b4663c8f5cf9dc37
Rui <86rui.mendes@gmail.com>
2022-12-06 14:45:15 +0000

    Added the resetEtch function which resets the grid with the button event listener

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const button = document.querySelector(".button");
 const size = 16;
 
 function createGrid(amountOfGrids) {
+    let outterBox = document.createElement("div");
+    outterBox.classList.add("outterBox");
     for (let i = 0; i < amountOfGrids; i++) {
         const row = document.createElement("div");
         row.classList.add("grid-row");
@@ -25,8 +27,25 @@ function createGrid(amountOfGrids) {
             });
             row.appendChild(gridBox);
         }
-        sketchContainer.appendChild(row);
+        outterBox.appendChild(row);
     }
+    sketchContainer.appendChild(outterBox);
 }
 
 createGrid(size);
+
+function resetEtch() {
+    button.addEventListener("click", () => {
+        let sizeChoice = Number(prompt("Type the desired size between 1 and 100"));
+        while (sizeChoice > 100) {
+            sizeChoice = Number(
+                prompt("You can only choose a number between 1 and 100")
+            );
+        }
+        let outterBox = document.querySelector(".outterBox");
+        outterBox.remove();
+        createGrid(sizeChoice);
+    });
+}
+
+resetEtch();
